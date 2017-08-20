214a7deb122e7554ad98422d7fb37732b93c2865
Brian Beardmore <bbeardmore@carvizor.com>
2017-08-19 19:43:50 -0500

    meVC ground work

---
+++ breakpoint/breakpoint/Controller/MeVC.swift
@@ -0,0 +1,26 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Brian Beardmore on 8/19/17.
+//  Copyright © 2017 Brian Beardmore. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func signOutButtonWasPressed(_ sender: Any) {
+        //sign out code goes here
+    }
+
+}
