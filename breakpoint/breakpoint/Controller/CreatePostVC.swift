a8dde5d46ff7748c6a6e00ec271e8d2ab977616e
Brian Beardmore <bbeardmore@carvizor.com>
2017-08-20 09:19:25 -0500

    first post to Firebase!

---
+++ breakpoint/breakpoint/Controller/CreatePostVC.swift
@@ -0,0 +1,57 @@
+//
+//  CreatePostVC.swift
+//  breakpoint
+//
+//  Created by Brian Beardmore on 8/20/17.
+//  Copyright © 2017 Brian Beardmore. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendButtonWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendButton.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!,withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendButton.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendButton.isEnabled = true
+                    print("There was an error sending post")
+                }
+            })
+        }
+    }
+    
+    @IBAction func closeButtonWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
+
+
+
+
